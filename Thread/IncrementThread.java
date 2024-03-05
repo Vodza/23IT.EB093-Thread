e291b0ce7eeb1d62bc6eebf07c7ddce48a7b81e0
Tran Thang <tranthang090406@gmail.com>
2024-03-05 23:43:44 +0700

    first commit

---
+++ Thread/IncrementThread.java
@@ -0,0 +1,22 @@
+public class IncrementThread extends Thread {
+
+    private Counter counter;
+
+    private int incrementsPerThread;
+
+    public IncrementThread(Counter counter, int incrementsPerThread) {
+        this.counter = counter;
+        this.incrementsPerThread = incrementsPerThread;
+    }
+
+    @Override
+    public void run() {
+        for(int i=0;i<incrementsPerThread;i++){
+            try {
+                counter.increment();
+            } catch (Exception e) {
+                throw new RuntimeException();
+            }
+        }
+    }
+}
